1327197085e01697d8fa22d4d922dca655e2ae8b
Max Potásio <simarxpotasio@gmail.com>
2020-08-21 13:07:38 -0300

    lesson: Input usability.

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -16,7 +16,7 @@ const Input: React.FC<InputProps> = ({name, icon: Icon, ...rest}) => {
   const {fieldName, defaultValue, error, registerField} = useField(name);
 
   const handleInputFocus = useCallback(() => {
-    setIsFocused(true)
+    setIsFocused(true);
   }, []);
 
   const handleInputBlur = useCallback(() => {
